dd64183eb743b755d39f8af4f487209f078f40a9
Andrei Florin Criste <Andrei-Florin.Criste@endava.com>
2023-02-21 21:36:31 +0200

    FizzBuzz procedural

--- FizzBuzz/FizzBuzz/FizzBuzzGenerator.cs
+++ FizzBuzz/FizzBuzz/FizzBuzzGenerator.cs
@@ -1,18 +1,46 @@
-﻿using FizzBuzz.Rules;
-
+﻿
 namespace FizzBuzz;
 
-public class FizzBuzzGenerator
-{
-    private readonly FizzBuzzRule _fizzBuzzRule;
+public class FizzBuzzGenerator {
+    private const string Fizz = "Fizz";
+    private const string Buzz = "Buzz";
+    private const string Even = "Even";
+    private const string Prime = "Prime";
 
-    public FizzBuzzGenerator() => _fizzBuzzRule = new PrimeFizzBuzzRule(new EvenFizzBuzzRule(new ThreeAndFiveFizzBuzzRule()));
-
-    public IEnumerable<string> Get(int count)
-    {
+    public IEnumerable<string> Get(int count) {
         return Enumerable.Range(1, count)
-            .Select(_fizzBuzzRule.Next)
-            .Where(e => e != null)
-            .Cast<string>();
+            .Select(FizzBuzzText);
+    }
+
+    private string FizzBuzzText(int value) {
+        if (value.IsPrime()) {
+            return Prime;
+        }
+
+        if (value % 3 == 0 && value % 5 == 0) {
+            return EvenPrefix(value, Fizz + Buzz);
+        }
+
+        if (value % 3 == 0) {
+            return EvenPrefix(value, Fizz);
+        }
+
+        if (value % 5 == 0) {
+            return EvenPrefix(value, Buzz);
+        }
+
+        return CheckForEvenOrText(value);
+    }
+
+    public string EvenPrefix(int value, string text) {
+        if (value % 2 == 0) return Even + text;
+
+        return text;
+    }
+
+    public string CheckForEvenOrText(int value) {
+        if (value % 2 == 0) return Even;
+
+        return value.ToString();
     }
 }
